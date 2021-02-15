98ed711cd122cc645057bf032af4a35a3504ad42
Thad Cole <thadjcole@gmail.com>
2021-02-14 22:52:15 -0500

    using dotenv for environment variables for heroku deployment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const Discord = require("discord.js");
-const config = require("./config.json");
 const client = new Discord.Client();
+
+// load env variables
+const { channelId, prefix, token } = process.env;
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -9,11 +12,11 @@ client.once("ready", () => {
 client.on("message", (message) => {
   // if the message is in the tech-support channel and starts with HELLO IT
   if (
-    message.channel.id === config.channelId &&
+    message.channel.id === channelId &&
     message.content.startsWith("HELLO IT")
   ) {
     message.channel.send("Have you tried turning it off and on again?");
   }
 });
 
-client.login(config.token);
+client.login(token);
